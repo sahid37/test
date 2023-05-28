156418f3ba9a6de492cd51d45e9a27a967e6cdfa
sahid <sahid37@gmail.com>
2023-05-28 13:38:36 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,10 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  ViewChild,
-  ViewContainerRef,
-} from '@angular/core';
-import { NewsService } from './service/news.service';
-import { ContainerRef } from 'ngx-infinite-scroll';
-import { Articles } from './service/news';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  // @ViewChild('scroll-container') container!: Element;
-  articles: Articles[] = [];
-  more = false;
-  start = 1;
-  pageSize = 10;
-  articlesAsChunks: Articles[] = [];
-
-  constructor(private news: NewsService) {}
-
-  ngOnInit(): void {
-    this.getHeadlines();
-  }
-
-  /**
-   * Function to get news headline articles
-   * @returns { void }
-   */
-  getHeadlines(): void {
-    this.news
-      .getTopHeadlines(this.pageSize)
-      .toPromise()
-      .then(
-        (results: any) => {
-          this.articles = results.articles;
-          this.addItems(this.start, this.pageSize);
-          console.log(this.articles);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-  }
-
-  /**
-   * Function to load specified number of articles
-   * @param { number } index start index
-   * @param { number } pageSize end index of the loaded data
-   * @returns { void }
-   */
-  addItems(index: number, pageSize: number): void {
-    for (let i = index; i < pageSize; ++i) {
-      this.articlesAsChunks.push(this.articles[i]);
-      console.log(this.articles);
-    }
-  }
-
-  /**
-   * Function to load articles on scroll
-   * @returns { void }
-   */
-  onScroll(): void {
-    this.start = this.pageSize;
-    this.pageSize += this.pageSize;
-    this.getHeadlines();
-  }
+export class AppComponent {
+  title = 'test';
 }
--- src/app/pipe/replace-null.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReplaceNullPipe } from './replace-null.pipe';
-
-describe('ReplaceNullPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReplaceNullPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
